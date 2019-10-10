01b06b0b532a71a4aa0247eeba913ff4c414098f
diegopereira99 <53828569+diegopereira99@users.noreply.github.com>
2019-10-10 14:18:45 -0300

    Update Passageiro.php

--- 2015/Elevador/Passageiro.php
+++ 2015/Elevador/Passageiro.php
@@ -2,12 +2,20 @@
 class Passageiro{
 	private $nome;
 	private $codigo;
-	
+	private $idade;
+		
 	public function setCodigo($codigo){
 		$this->codigo = $codigo;
 	}
 	public function getCodigo(){
 		return $this->codigo;
+	}
+	
+	public function setIdade($idade) {
+		$this->idade = $idade;
+	}
+	public function getIdade() {
+		return $this->idade;
 	}
 	
 	public function setNome($nome){
